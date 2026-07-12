f444aaa33ee2f221cb09e8b901565c3d22a7ecf1
Mohammed Yasser <mohammedyasser199713@gmail.com>
2026-07-12 17:52:09 +0300

    55

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.JPG', '**/*.JPEG', '**/*.PNG'],
   css: {
     postcss: './postcss.config.js', // Make sure this points to the right config
   },
